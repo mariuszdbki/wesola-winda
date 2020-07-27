93a90881372bab75ed0ccb7076bfe5e8cf5b4dcb
Mariusz Dąbrowski <mariuszdbki@gmail.com>
2020-07-28 00:16:15 +0200

    + basic working principles, without LEDs

--- wesola-winda.ino
+++ wesola-winda.ino
@@ -17,10 +17,6 @@ const int pinLedBtnDown = 14;
 bool liftRunningUp = false;
 bool liftRunningDown = false;
 
-// czy zawołana
-bool liftCalledUp = false;
-bool liftCalledDown = false;
-
 // czy jest na górze / dole
 bool liftUp = false;
 bool liftDown = false;
@@ -38,9 +34,127 @@ void setup() {
   pinMode(pinLedCallDown, OUTPUT);
   pinMode(pinLedBtnUp, OUTPUT);
   pinMode(pinLedBtnDown, OUTPUT);
+  stopLift();
+  setLiftPosition();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (liftRunningUp) {
+    if (reachedStopUp()) {
+      stopLift();
+      setLiftUp();
+    }
+    else if (manualStop()) {
+      stopLift();
+      setLiftMiddle();
+    }
+  }
+  else if (liftRunningDown) {
+    if (reachedStopDown()) {
+      stopLift();
+      setLiftDown();
+    }
+    else if (manualStop()) {
+      stopLift();
+      setLiftMiddle();
+    }
+  }
+  else { // lift stopped
+    if (!liftUp && (calledUp() || pressedBtnUp())) {
+      startLiftUp();
+    }
+    else if (!liftDown && (calledDown() || pressedBtnDown())) {
+      startLiftDown();
+    }
+  }
 
+  // @todo: obsłużyć ledy i reakcje na przyciski "call" gdy winda jedzie - a dokładnie brak tej reakcji (czyli tylko migać ledami)
+}
+
+void setLiftPosition() {
+  if (reachedStopUp()) {
+    setLiftUp();
+  }
+  else if (reachedStopDown()) {
+    setLiftDown();
+  }
+  else {
+    setLiftMiddle();
+  }
+}
+
+bool reachedStopUp() {
+  return digitalRead(pinStopUp) == HIGH;
+}
+
+bool reachedStopDown() {
+  return digitalRead(pinStopDown) == HIGH;
+}
+
+bool manualStop() {
+  return pressedBtnUp() || pressedBtnDown();
+}
+
+void stopLift() {
+  digitalWrite(pinLiftUp, LOW);
+  digitalWrite(pinLiftDown, LOW);
+}
+
+void setLiftUp() {
+  liftUp = true;
+  liftDown = false;
+}
+
+void setLiftDown() {
+  liftUp = false;
+  liftDown = true;
+}
+
+void setLiftMiddle() {
+  liftUp = false;
+  liftDown = false;
+}
+
+// call and lift buttons working on reverse logic
+
+bool calledUp() {
+  return digitalRead(pinCallUp) == LOW;
+}
+
+bool calledDown() {
+  return digitalRead(pinCallDown) == LOW;
+}
+
+bool pressedBtnUp() {
+  return digitalRead(pinBtnUp) == LOW;
+}
+
+bool pressedBtnDown() {
+  return digitalRead(pinBtnDown) == LOW;
+}
+
+bool canStartUp() {
+  return !liftUp && !liftRunningUp && !liftRunningDown && !reachedStopUp();
+}
+
+bool canStartDown() {
+  return !liftDown && !liftRunningDown && !liftRunningUp && !reachedStopDown();
+}
+
+void startLiftUp() {
+  if (canStartUp()) {
+    liftRunningUp = true;
+    liftRunningDown = false;
+    digitalWrite(pinLiftDown, LOW);
+    digitalWrite(pinLiftUp, HIGH);
+  }
+}
+
+void startLiftDown() {
+  if (canStartDown()) {
+    liftRunningUp = false;
+    liftRunningDown = true;
+    digitalWrite(pinLiftUp, LOW);
+    digitalWrite(pinLiftDown, HIGH);
+  }
 }
